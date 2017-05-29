01c74f172313c443173f530750675b5611a89fa6
cowiefr <cowiefr@telus.net>
2017-05-29 10:44:10 -0600

    First Commit

---
+++ CameraVC.swift
@@ -0,0 +1,19 @@
+//
+//  CameraVC.swift
+//  DevChat
+//
+//  Created by Fain Cowie on 2017-05-29.
+//  Copyright © 2017 Fain Cowie. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
